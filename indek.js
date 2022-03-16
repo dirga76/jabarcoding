1e1842fec70de17b975abf5395380de2eb195b08
dirga76 <87862021+dirga76@users.noreply.github.com>
2022-03-16 12:49:50 +0700

    Add files via upload

--- indek.js
+++ indek.js
@@ -1,41 +1,20 @@
-//ALDO febrian d.m
-// soal 1
+// di index.js
+var readBooks = require("./callback.js");
 
-const luas = (x, y) => x * y;
-const keliling = (x, y) => 2 * (x + y);
-console.log("hitung luas dan keliling persegi panjang : ");
-console.log(luas(5, 2));
-console.log(keliling(5, 2));
+var books = [
+    { name: "LOTR", timeSpent: 3000 },
+    { name: "Fidas", timeSpent: 2000 },
+    { name: "Kalkulus", timeSpent: 4000 },
+    { name: "komik", timeSpent: 1000 },
+];
 
-// soal 2
-const fullNamee = {
-    firstNamee: "William",
-    lastNamee: "Imoh",
-};
-const { firstNamee, lastNamee } = fullNamee;
-console.log(firstNamee, lastNamee);
-
-// soal 3
-const newObject = {
-    firstName: "Muhammad",
-    lastName: "Iqbal Mubarok",
-    address: "Jalan Ranamanyar",
-    hobby: "playing football",
-};
-const { firstName, lastName, address, hobby } = newObject;
-
-console.log(firstName, lastName, address, hobby);
-
-// soal 4
-const west = ["Will", "Chris", "Sam", "Holly"];
-const east = ["Gill", "Brian", "Noel", "Maggie"];
-const combined = [...west, ...east];
-console.log(combined);
-
-// soal 5
-const planet = "earth";
-const view = "glass";
-// var before = 'Lorem ' + view + 'dolor sit amet, ' + 'consectetur adipiscing elit,' + planet
-const theString =
-    "Lorem ${view} dolor sit amet, consectetur adipiscing elit ${planet}";
-console.log(theString);
+function baca(time, books, i) {
+    if (i < books.length) {
+        readBooks(time, books[i], function(sisa) {
+            if (sisa > 0) {
+                i += 1;
+                baca(sisa, books, i);
+            }
+        });
+    }
+}
